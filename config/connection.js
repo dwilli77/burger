e6b94ca787a6e6aee51af46760846c152ee763f9
Derek W <41767875+dwilli77@users.noreply.github.com>
2019-01-20 12:25:24 -0500

    added JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.host,
-  port: process.env.port,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: process.env.password,
+		database: 'burgers_db'
+	})
+};
 
 connection.connect(function(err) {
   if (err) {
